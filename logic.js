e6baef564edb45907297e0f6e4a4db0673350f31
Jancee <jancee800@gmail.com>
2024-02-26 16:54:11 +0800

    write with color

--- logic.js
+++ logic.js
@@ -1,6 +1,13 @@
 const colorInput = document.getElementById('color-picker')
 let color = colorInput.value
 
+const penButton = document.getElementById('pen')
+const rainbowButton = document.getElementById('rainbow')
+const eraserButton = document.getElementById('eraser')
+const clearButton = document.getElementById('clear')
+let colorMode = "pen"
+let isDrawing = "false"
+
 const canvas = document.getElementById('canvas')
 const generateButton = document.getElementById('generate-canvas')
 
@@ -18,6 +25,7 @@ generateButton.addEventListener('click', () => {
     canvas.innerHTML = ''
     generateDivs(canvasSize)
     console.log(color)
+    isDrawing = "false"
 })
 
 function generateDivs(size) {
@@ -30,9 +38,28 @@ function generateDivs(size) {
         div.style.width = cellSize + "px"
         div.style.height = cellSize + "px"
 
-        div.style.border = "solid black"
         div.style.boxSizing = "border-box"
         div.style.flexGrow = "1"
+
+        div.addEventListener('mousedown', () => {
+            if (colorMode === "pen") {
+                isDrawing = "true"
+                div.style.backgroundColor = color
+            }
+        })
+
+        div.addEventListener('mousemove', () => {
+            if (colorMode === "pen") {
+                if (isDrawing == "true") {
+                    div.style.backgroundColor = color
+                }
+            }
+        })
+
+        div.addEventListener('mouseup', () => {
+            isDrawing = "false"
+        })
+
         canvas.appendChild(div)
     }
 }
@@ -40,4 +67,13 @@ function generateDivs(size) {
 
 colorInput.addEventListener('input', () => {
     color = colorInput.value
+    colorMode = "pen"
+})
+
+penButton.addEventListener('click', () => {
+    colorMode = "pen"
+})
+
+rainbowButton.addEventListener('click', () => {
+    colorMode = "rainbow"
 })
